3b75502edf685567e48cb3950d78fd762536b80a
kaanyalti <yaltikaan@gmail.com>
2021-08-15 04:28:01 -0400

    chapter 4 append, added main function

--- ch04_composite_types/append/main.go
+++ ch04_composite_types/append/main.go
@@ -1,6 +1,8 @@
 package main
 
-func appendInt(x []int, y int) {
+import "fmt"
+
+func appendInt(x []int, y int) []int {
 	var z []int
 	zlen := len(x) + 1
 	if zlen <= cap(x) {
@@ -18,4 +20,13 @@ func appendInt(x []int, y int) {
 	}
 	z[len(x)] = y
 	return z
+}
+
+func main() {
+	var x, y []int
+	for i := 0; i < 10; i++ {
+		y = appendInt(x, i)
+		fmt.Printf("%d cap=%d\t%v\n", i, cap(y), y)
+		x = y
+	}
 }
